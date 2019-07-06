785104b382bb54c082bbdc530c15460c9d91aaa0
andrew macbook <andrewCheng078@gmail.com>
2019-07-06 20:44:47 +0800

    release 1.0

--- Plugin.js
+++ Plugin.js
@@ -24,148 +24,173 @@
 //         console.log('whenTransition');
 //     }
 
-(function($) {
+(function ($) {
     'use strict';
-        var ModuleName = 'lbx_lnop';
-    
-        var Module = function ( ele, options ) {
-            this.ele = ele;
-            this.$ele = $(ele);
-            this.option = options;
-        };
-    
-        Module.DEFAULTS = {
-            openAtStart:true,
-            autoToggle:false,//open === true toggle will
-            transition: true,
-            whenClickCallback: function() {
-                console.log('whenClickCallback,DEFAULTS');
-            }
-        };
+    var ModuleName = 'lbx_lnop';
 
-        Module.prototype.init = function ( opts ) {
-            var jQuery = this.$ele;
-            $('.banner').append(` <button class="wrap_btn"> 收合</button>`)
-            $('.wrap_btn').click(function () {
-                $('.banner').lbx_lnop('toggle');
-            })
-            this.transition(opts);
-            if(opts.transition){ jQuery.addClass('transition')};
-            if(opts.openAtStart){this.open(opts.openAtStart)}else{this.close(opts.openAtStart)};
-            if(opts.autoToggle){this.toggle(opts.autoToggle)};
-     
-           
-            
-            console.log('this is init');
-        };
-        Module.prototype.transition = function ( opts ) {
-          
-        //     var whenTransition = setInterval(() => {
-        //        opts.whenTransition;
-        //     }, 50);
-        //     $('.banner').on('transitionend webkitTransitionEnd oTransitionEnd', function () {
-        //         clearInterval(whenTransition);
-        //    });
-        console.log(' this.transition(opts);')
-          
-        };
-        
-        Module.prototype.toggle = function ( opts ) {
-            var jQuery = this.$ele;
-            const bannerHeight = jQuery.css('height') >= '80px';
-            bannerHeight ? this.open( opts) : this.close( opts);
-        };
+    var Module = function (ele, options) {
+        this.ele = ele;
+        this.$ele = $(ele);
+        this.option = options;
+        this.class = null;
+        this.whenClickCallback = "";
 
-        Module.prototype.open = function ( opts ) {
-            var jQuery = this.$ele;
-            
-    
-            typeof(opts)==="number" ? setTimeout(function(){ open()},opts) : open();
-         
-            function open(){
-                // jQuery.removeClass("closed");
-                // jQuery.addClass("opened");
-                // var hasopened = $('.banner').hasClass("opened");
-                // if(!hasopened){jQuery.addClass("opened");}
-                jQuery.removeClass("closed");
+    };
+
+    Module.DEFAULTS = {
+        openAtStart: true,
+        autoToggle: 3000, //open === true toggle will
+        transition: true,
+        whenClickCallback: function () {
+            console.log('whenClickCallback,DEFAULTS');
+        }
+    };
+
+    Module.prototype.init = function (opts) {
+        var jQuery = this.$ele;
+        $('.banner').append(` <button class="wrap_btn"> 收合</button>`)
+        $('.wrap_btn').click(function () {
+            $('.banner').lbx_lnop('toggle');
+        })
+        if (opts.transition) {
+            jQuery.addClass('transition')
+        };
+        if (opts.openAtStart) {
+            this.open(opts.openAtStart)
+        } else {
+            this.close(opts.openAtStart)
+        };
+        if (opts.autoToggle) {
+            this.toggle(opts.autoToggle)
+        };
+        this.whenClickCallback = opts.whenTransition;
+
+        //clear all interval
+        for (var i = 1; i < 99999; i++)
+        window.clearInterval(i);
+
+        console.log('this is init');
+    };
+    Module.prototype.transition = function (opts) {
+        console.log(opts)
+        var whenTransition = setInterval(() => {
+            this.whenClickCallback();
+        }, 25);
+        $('.banner').on('transitionend ', function () {
+            clearInterval(whenTransition);
+        });
+
+
+    };
+
+    Module.prototype.toggle = function (opts) {
+        var jQuery = this.$ele;
+        const bannerHeight = jQuery.css('height');
+
+        if (bannerHeight >= '80px') {
+            this.open(opts);
+            this.transition();
+        } else {
+            this.close(opts);
+            this.transition();
+        }
+
+    };
+
+    Module.prototype.open = function (opts) {
+
+        var jQuery = this.$ele;
+
+        if (typeof (opts) === "number") {
+            setTimeout(() => {
+                this.class = "opened";
                 jQuery.addClass("opened");
+                jQuery.removeClass("closed");
+            }, opts)
+        } else {
 
-//  $('.banner').on('transitionend webkitTransitionEnd oTransitionEnd', function () {
-//                var hasopened = $('.banner').hasClass("opened");
-//                var hasopening = $('.banner').hasClass("opening");
-            
-               
-//                 if(hasopening){
-//                     jQuery.removeClass("opening");
-//                     jQuery.addClass("opened");
-//                 }
-//             });
-
-                $('.wrap_btn').text('收合')
+            if (this.class === null) {
+                this.class = "opened";
+                jQuery.addClass("opened");
+            } else if (this.class === "closed") {
+                this.class = "opening";
+                jQuery.addClass("opening")
+                jQuery.removeClass("closed")
             }
-          
-            console.log('this is open!!!:', opts);
-        };
-       
-        Module.prototype.close = function ( opts ) {
-            var jQuery = this.$ele;
-            this.transition;
-            typeof(opts)==="number" ? setTimeout(function(){ close()},opts) : close();
-          
-          function close(){
-            jQuery.addClass("closing");
-            $('.banner').on('transitionend webkitTransitionEnd oTransitionEnd', function () {
-               var hasclosing = $('.banner').hasClass("closing");
-               var hasopening = $('.banner').hasClass("opening");
-               console.log(hasclosing)
-                if( hasclosing ){
-                    jQuery.removeClass("closing");
-                    jQuery.addClass("closed");
-                }
-                if(hasopening){
-                    jQuery.removeClass("opening");
-                    jQuery.addClass("opened");
-                }
-            });
-           jQuery.removeClass("opened");
-            $('.wrap_btn').text('展開')
-          }
+        }
 
-            console.log('this is close!!!:', opts);
-            
-        };
-    
 
-        $.fn[ModuleName] = function ( methods, options ) {
-            return this.each(function(){
-                var $this = $(this);
-                var module = $this.data( ModuleName );
-                var opts = null;
-                if ( !!module ) {
-                    if ( typeof methods === 'string' &&  typeof options === 'undefined' ) {
-                        // console.log(' module[methods]();')
-                        module[methods]();
-                    } else if ( typeof methods === 'string' &&  typeof options === 'object' ) {
-                        module[methods](options); 
-                        //  console.log('module[methods](options)');
-                    } else {
-                        console.log('unsupported options!');
-                        throw 'unsupported options!';
-                    }
+        $('.banner').on('transitionend ', () => {
+            if (this.class === "opening") {
+                this.class = "opened";
+                jQuery.addClass("opened")
+                jQuery.removeClass("opening")
+            }
+        });
+        $('.wrap_btn').text('收合');
+
+        console.log('this is open!!!:', opts);
+
+    };
+
+    Module.prototype.close = function (opts) {
+        var jQuery = this.$ele;
+        // typeof(opts)==="number" ? setTimeout(function(){ close()},opts) : close();
+        if (typeof (opts) === "number") {
+            setTimeout(() => {
+                this.class = "closed";
+                jQuery.addClass("closed");
+                jQuery.removeClass("opened");
+            }, opts)
+        } else {
+            if (this.class === null) {
+                this.class = "closed";
+                jQuery.addClass("closed")
+            } else if (this.class === "opened") {
+                this.class = "closing";
+                jQuery.addClass("closing")
+                jQuery.removeClass("opened")
+            }
+        }
+
+        $('.banner').on('transitionend ', () => {
+            if (this.class === "closing") {
+                this.class = "closed";
+                jQuery.addClass("closed")
+                jQuery.removeClass("closing")
+            }
+        });
+
+        $('.wrap_btn').text('展開')
+
+        console.log('this is close!!!:', opts);
+
+    };
+
+
+    $.fn[ModuleName] = function (methods, options) {
+        return this.each(function () {
+            var $this = $(this);
+            var module = $this.data(ModuleName);
+            var opts = null;
+            if (!!module) {
+                if (typeof methods === 'string' && typeof options === 'undefined') {
+                    // console.log(' module[methods]();')
+                    module[methods]();
+                } else if (typeof methods === 'string' && typeof options === 'object') {
+                    module[methods](options);
+                    //  console.log('module[methods](options)');
                 } else {
-                    opts = $.extend( {}, Module.DEFAULTS, ( typeof methods === 'object' && options ), ( typeof options === 'object' && options ) );
-                    module = new Module(this, opts);
-                    $this.data( ModuleName, module );
-                    module.init(  methods );
+                    console.log('unsupported options!');
+                    throw 'unsupported options!';
                 }
-            });
-        };
-    
-    })(jQuery);
+            } else {
+                opts = $.extend({}, Module.DEFAULTS, (typeof methods === 'object' && options), (typeof options === 'object' && options));
+                module = new Module(this, opts);
+                $this.data(ModuleName, module);
+                module.init(methods);
+            }
+        });
+    };
 
-
-
-
-
-
-
+})(jQuery);
